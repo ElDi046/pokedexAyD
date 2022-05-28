a9d2cb8abf3d0898235074addaa4a06e9564d90c
Aldo Ortiz <aortizm@live.com.mx>
2022-05-27 23:57:12 -0500

    Preguntas resueltas
    Últimas preguntas respondidas

--- app/src/main/java/di/dev/pokedex/MainActivity.kt
+++ app/src/main/java/di/dev/pokedex/MainActivity.kt
@@ -12,6 +12,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import di.dev.pokedex.ui.theme.PokedexTheme
 
+/*
+¿definir un renglón en xml? -> Es correcto, debemos de definir que se va a utilizar un ListView
+¿tener una clase Adapter y una clase ViewHolder? -> Es correcto, necesitamos el adapter para poder mostrar la información dentro de nuestra aplicación
+¿Se requiere de una lista de datos? -> Es correcto, necesitamos la lista donde se obtendrá la información para mostrarla.
+¿en qué hilo se crea la lista? -> Se crea un hilo diferente al Main Thread para que pueda hacer funciones asincronas dentro de la aplicación.
+Algunas de estas funciones puede ser obtener la información de la API.
+
+ */
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
